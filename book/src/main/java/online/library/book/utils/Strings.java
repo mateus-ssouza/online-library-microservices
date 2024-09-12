cd2b8fd57c30934dfedac216ac1a61aaa405c59d
Mateus <msouzacrv@gmail.com>
2024-09-12 12:46:10 -0300

    :sparkles: feat: utils and exceptions files

---
+++ book/src/main/java/online/library/book/utils/Strings.java
@@ -0,0 +1,14 @@
+package online.library.book.utils;
+
+public abstract class Strings {
+
+    public static final class BOOK {
+        public static final String ERROR_FIND_ALL_LIST = "Error while retrieving the list of books.";
+        public static final String NOT_FOUND = "Book not found.";
+        public static final String CONFLICT = "Book's CPF, email, or login already in use.";
+        public static final String ERROR_FIND_BY_ID = "Error while trying to retrieve a book by ID.";
+        public static final String ERROR_CREATE = "Error while trying to create a book.";
+        public static final String ERROR_UPDATE = "Error while trying to update a book.";
+        public static final String ERROR_DELETE = "Error while trying to delete a book.";
+    }
+}
